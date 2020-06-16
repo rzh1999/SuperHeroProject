633a12eeb13cec9cd529c926260451cdbfd8347d
Bill <william.hine2019@gmail.com>
2020-06-16 15:11:53 -0400

    Added properties to Model

---
+++ SuperHero/Models/SuperHeroModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHero.Models
+{
+    public class SuperHeroModel
+    {
+        [Key]
+        public int Id { get; set; }
+        private string name { get; set; }
+        private string alterEgo { get; set; }
+        private string primaryAbility { get; set; }
+        private string secondaryAbility { get; set; }
+        private string catchPhrase { get; set; }
+
+    }
+}
