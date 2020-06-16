8e9da085548a0a53c26da7d5db2bd74e6a5286e5
Bill <william.hine2019@gmail.com>
2020-06-16 15:17:29 -0400

    Added Superheromodel and set bd context

--- SuperHero/Data/ApplicationDbContext.cs
+++ SuperHero/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHero.Models;
 
 namespace SuperHero.Data
 {
@@ -12,5 +13,7 @@ namespace SuperHero.Data
             : base(options)
         {
         }
+
+        public DbSet<SuperHeroModel> SuperHeroes { get; set; }
     }
 }
