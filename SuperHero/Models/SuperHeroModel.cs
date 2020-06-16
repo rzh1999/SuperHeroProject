36d1a08f2f676aff15ea82f18e79912e19d140a3
Bill <william.hine2019@gmail.com>
2020-06-16 18:30:57 -0400

    Brought back DisplayNameFor() added display to coulmns instead

--- SuperHero/Models/SuperHeroModel.cs
+++ SuperHero/Models/SuperHeroModel.cs
@@ -11,10 +11,19 @@ namespace SuperHero.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name ="Name")]
         public string name { get; set; }
+        [Display(Name = "Alter Ego")]
         public string alterEgo { get; set; }
+
+        [Display (Name = "Primary Ability")]
         public string primaryAbility { get; set; }
+
+        [Display (Name = "Secondary Ability")]
         public string secondaryAbility { get; set; }
+
+        [Display (Name = "Catch Phrase")]
         public string catchPhrase { get; set; }
 
     }
